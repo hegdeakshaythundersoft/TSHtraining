f0fa0ea5d1e2827f950c76bfb9e81624b584c6b9
Akshay_Hegde <hegdeakshay320@gmail.com>
2021-10-13 18:32:46 +0530

    first_test_app_tried_on_own_referred_the_codes_given

---
+++ header.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <getopt.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/select.h>
+#include <sys/time.h>
+
+
+#include <linux/videodev2.h>
+#include "videodev2-fsr172x.h"
+
+#ifndef V4L2_BUF_FLAG_ERROR
+#define V4L2_BUF_FLAG_ERROR	0x0040
+#endif
+
+#define ARRAY_SIZE(a)	(sizeof(a)/sizeof((a)[0]))
+
---
+++ my_first_video_test.c
@@ -0,0 +1,70 @@
+#include "header.h" 
+
+typedef struct video_device{
+
+	int fd;
+
+
+}device;
+
+device my_video_device;
+
+//static int video_device_open(device*,const char*,int);
+//static int video_device_close(device*);
+static int video_device_open(device *dev,const char* name,int no_request){
+	dev->fd=open(name,O_RDWR);
+	if((dev->fd)>=0)
+	printf("\nVideo opened successfully\n");
+	else
+	printf("Can't open the video");
+
+
+}
+
+static int video_device_close(device *dev){
+
+	close(dev->fd);
+
+
+}
+
+
+int main(int argc,char* argv[]){
+	
+	int ret;
+	char *device_name="/dev/video0";
+	printf("\nCalling the open function\n");
+	ret=video_device_open(&my_video_device,device_name,0);
+	if(!(ret<0))
+	printf("\nSUCCESSFULLY OPENED\n");
+	else
+	printf("\nFAILED TO OPEN\n");
+	video_device_close(&my_video_device);
+	printf("\nClosed the sesion\n");
+	return 0;
+}
+
+
+/*
+
+akshay@akshay-ThinkPad-L570-W10DG:~/video_module/akshay$ make
+gcc -Iinclude/linux    my_first_video_test.c   -o my_first_video_test
+akshay@akshay-ThinkPad-L570-W10DG:~/video_module/akshay$ ./my_first_video_test 
+
+Calling the open function
+
+Video opened successfully
+
+SUCCESSFULLY OPENED
+
+Closed the sesion
+akshay@akshay-ThinkPad-L570-W10DG:~/video_module/akshay$ 
+
+
+
+*/
+
+
+
+
+
